ce10fea75c4c14bb47a692c497588b3de59a0c12
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-08 23:39:59 +0200

    Expand item model with user reference and expiry date tracking

--- back-end/models/Item.js
+++ back-end/models/Item.js
@@ -1,12 +1,23 @@
 import mongoose from 'mongoose';
 
 const itemSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: false 
+  },
   name: {
     type: String,
     required: true,
+    trim: true
   },
   category: {
     type: String,
+    enum: ['Fridge', 'Freezer', 'Pantry'],
+  },
+  expiryDate: {
+    type: Date,
+    required: true,
   },
   quantity: {
     type: Number,
