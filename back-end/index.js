85a0449e618a61318ca8f858564a280bc1884646
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-08 18:04:53 +0200

    Add user model

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 import connectDB from './config/db.js';
+import User from './models/User.js';
 
 dotenv.config();
 
