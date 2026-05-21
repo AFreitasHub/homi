12449046cf45e8c1aaafc49af0bfca1ee9746f24
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-21 20:24:01 +0200

    Update userRoutes.js

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/userController.js';
+import { registerUser, loginUser, getUserProfile, updateUserProfile, deleteUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', registerUser);
 
 router.post('/login', loginUser);
+
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
+    .delete(protect, deleteUserProfile);
 
 export default router;
