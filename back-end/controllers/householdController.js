aaa71153735931ea5fd6117155d04271c7c19169
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-21 20:03:38 +0200

    Add household joining controller logic

--- back-end/controllers/householdController.js
+++ back-end/controllers/householdController.js
@@ -26,3 +26,32 @@ export const createHousehold = async (req, res) => {
         res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
+
+export const joinHousehold = async (req, res) => {
+    try {
+        const { inviteCode } = req.body; 
+
+        if (!inviteCode) {
+            return res.status(400).json({ message: 'Invite code is required' });
+        }
+        
+        const household = await Household.findOne({ inviteCode: inviteCode.toUpperCase() });
+
+        if (!household) {
+            return res.status(404).json({ message: 'Invalid invite code' });
+        }
+
+        if (household.members.includes(req.user._id)) {
+            return res.status(400).json({ message: 'You are already a member of this household' });
+        }
+
+        household.members.push(req.user._id);
+        await household.save();
+
+        await User.findByIdAndUpdate(req.user._id, { household: household._id });
+
+        res.json({ message: 'Successfully joined household', household });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+}
