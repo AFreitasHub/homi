1ec9659fa2c7948e10424a56b2fe491495163321
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-21 20:21:34 +0200

    Add delete user controller logic

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -97,3 +97,22 @@ export const updateUserProfile = async (req, res) => {
     }
 };
 
+export const deleteUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if (user.household) {
+            await Household.findByIdAndUpdate(user.household, {
+                $pull: { members: user._id }
+            });
+        }
+
+        await user.deleteOne();
+        res.json({ message: 'User account successfully removed' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
