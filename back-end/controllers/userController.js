db09fbecf1ee737ef53762194e25e475f75db36a
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-21 20:21:18 +0200

    Add update user controller logic

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -69,3 +69,31 @@ export const getUserProfile = async (req, res) => {
     }
 };
 
+export const updateUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id);
+
+        if (user) {
+            user.name = req.body.name || user.name;
+            user.email = req.body.email || user.email;
+            
+            if (req.body.password) {
+                user.password = req.body.password;
+            }
+
+            const updatedUser = await user.save();
+            res.json({
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                household: updatedUser.household,
+                token: generateToken(updatedUser._id)
+            });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
