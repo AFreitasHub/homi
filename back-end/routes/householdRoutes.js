85a50660fabf5c8cf811d3ae7c5aeee25aa4408e
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-21 20:13:24 +0200

    Update householdRoutes.js

--- back-end/routes/householdRoutes.js
+++ back-end/routes/householdRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createHousehold } from '../controllers/householdController.js';
+import { createHousehold, getHousehold, joinHousehold } from '../controllers/householdController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', protect, createHousehold);
+
+router.post('/join', protect, joinHousehold);
+
+router.get('/', protect, getHousehold);
 
 export default router;
