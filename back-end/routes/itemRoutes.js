6dea7c4343b18bc0c261fb9369aba9d2ef6a5af9
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-23 09:42:01 +0200

    add validation to item routes

--- back-end/routes/itemRoutes.js
+++ back-end/routes/itemRoutes.js
@@ -1,15 +1,17 @@
 import express from "express";
+import { body } from 'express-validator';
 import { createItem, getItems, deleteItem, editItem } from "../controllers/itemController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { itemValidation, itemUpdateValidation } from '../validators/itemValidator.js';
 
 const router = express.Router();
 
-router.post('/', protect, createItem);
+router.post('/', protect, itemValidation, createItem);
 
 router.get('/', protect, getItems);
 
 router.delete('/:id', protect, deleteItem);
 
-router.put('/:id', protect, editItem);
+router.put('/:id', protect, itemUpdateValidation, editItem);
 
 export default router;
