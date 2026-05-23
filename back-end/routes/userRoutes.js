24e1d3f8d82906b8ad911ad8b00db40cc64f50b9
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-23 09:42:10 +0200

    add validation to user routes

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,16 +1,18 @@
 import express from 'express';
+import { body } from 'express-validator';
 import { registerUser, loginUser, getUserProfile, updateUserProfile, deleteUserProfile } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
+import { registerValidation, profileUpdateValidation } from '../validators/userValidator.js';
 
 const router = express.Router();
 
-router.post('/', registerUser);
+router.post('/', registerValidation, registerUser);
 
 router.post('/login', loginUser);
 
 router.route('/profile')
     .get(protect, getUserProfile)
-    .put(protect, updateUserProfile)
+    .put(protect, profileUpdateValidation, updateUserProfile)
     .delete(protect, deleteUserProfile);
 
 export default router;
