94b3708a6ba940d157332c1d93940dad892d4a97
Afonso Freitas <182527744+AFreitasHub@users.noreply.github.com>
2026-05-31 03:06:00 +0200

    Add api.js

---
+++ front-end/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'; 
+
+const api = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+  }
+};
+
+export default api;
